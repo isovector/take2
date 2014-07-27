62a163b02758d13ce7084cf92936680a2cf68e7c
MatthewMaclean <matthewcmaclean@gmail.com>
2014-07-26 21:38:34 -0400

    Fixed getting the most recent commit that's in the origin

--- daemon/daemon/scm/git.py
+++ daemon/daemon/scm/git.py
@@ -7,14 +7,17 @@ from daemon.utils import make_tempfile, cached_property
 
 class Git(SCMBase):
     def _get_commit_cwd(self):
-        r = envoy_run('git rev-parse origin/HEAD')
-        return r.std_out
+        r = envoy_run('git rev-list HEAD')
+        commits = r.std_out.split('\n')
+        for commit in commits:
+            print commit
+            r = envoy_run('git branch -r --contains %s' % commit)
+            if r.std_out != "":
+                return commit
+
+        return None
 
     def _get_original_file(self):
         relative_file_path = relpath(self.file_path, self._repo_path)
-        print 'git show %s:"%s"' % (self.commit, relative_file_path)
         r = envoy_run('git show %s:"%s"' % (self.commit, relative_file_path))
-        print "-----------------------"
-        print r.std_out
-        print "-----------------------"
         return make_tempfile(r.std_out)
