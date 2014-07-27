8b320b8c2bf13c0cea0238c80833f0615926558f
Sandy Maguire <sandy@sandymaguire.me>
2014-07-27 13:21:38 -0400

    Added UserModel

--- back/conf/evolutions/default/1.sql
+++ back/conf/evolutions/default/1.sql
@@ -5,9 +5,11 @@
 
 create table "Dad" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"name" VARCHAR(254) NOT NULL);
 create table "Snapshot" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"timestamp" VARCHAR(254) NOT NULL,"file" VARCHAR(254) NOT NULL,"user" INTEGER NOT NULL,"commit" VARCHAR(254) NOT NULL,"lines" VARCHAR(254) NOT NULL);
+create table "User" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"name" VARCHAR(254) NOT NULL,"email" VARCHAR(254) NOT NULL,"lastActivity" VARCHAR(254) NOT NULL);
 
 # --- !Downs
 
 drop table "Dad";
 drop table "Snapshot";
+drop table "User";
 
