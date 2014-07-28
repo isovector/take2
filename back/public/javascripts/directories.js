70b185d2c9725f7d1f210543f44d9220510cee55
Jeff Lee <ja6lee@uwaterloo.ca>
2014-07-27 21:17:05 -0400

    switching users to icons

--- back/public/javascripts/directories.js
+++ back/public/javascripts/directories.js
@@ -1,4 +1,4 @@
-var ROOTURL = "http://0.0.0.0:9000/"
+var ROOTURL = "http://localhost:9000/"
 
 frostbite.filter('isDirectory', function() {
     return function(input) {
@@ -39,43 +39,40 @@ frostbite.filter('isDirectory', function() {
 	//$scope.getDirectories();
     }
 
-    $scope.getDirectories = function() {
-	console.log("Getting data");
-	console.log("repo/" + $scope.filename);
-	$http.get("/repo/" + $scope.filename).success(function(data) {
-	     console.log("RESPONSE");
-	     console.log(data);
-	     $scope.items = data;
-	     //TODO: switch to real date.. or just remove
-	     for (var i = 0; i < $scope.items.length; i++) {
-                 $scope.items[i].editedlast = {"id":7, "time":new Date()};
-             }
-             $scope.getUsers();
-	});	
-    }
-
     $scope.getUsers = function() {
 	console.log("Getting users");
 	for (var i = 0; i < $scope.items.length; i++) {
-            // TODO: switch to actual number of users
+        // TODO: switch to actual number of users
 	    (function(i) {
 	        $http.get("/api/users/" + $scope.items[i].path).success(function(data) {
-		    console.log("data: " + data);
-		    console.log("I: " + i);
-                    console.log($scope.items[i]);
-		    $scope.items[i].users = data;
-                    var numUsers = $scope.items[i].users.length;
-                    if (numUsers >= 3) {
-		        $scope.items[i].currentUsers = $scope.items[i].users[0].name + " and " + (numUsers - 1) + " Others";
+				$scope.items[i].users = data;
+				var numUsers = $scope.items[i].users.length;
+				for (var j = 0; j < numUsers; j++) {
+					//(function(i,j) {
+						var emailLowerCase = $scope.items[i].users[j].email.toLowerCase();
+						//var emailLowerCase = "";
+						console.log(emailLowerCase);
+						var hashEmail = CryptoJS.MD5( emailLowerCase );  
+						console.log("EMAIL: " + hashEmail);
+						$scope.items[i].users[j].picture = "http://www.gravatar.com/avatar/" + hashEmail;
+						/*$http.get("http://www.gravatar.com/" + hashEmail + ".json").success(function(data) {
+							console.log(data);
+						}).error(function(data, status, headers, config) {
+							console.log(status);	
+						});*/
+					//}(i,j));
+				}
+				if (numUsers >= 3) {
+					$scope.items[i].currentUsers = $scope.items[i].users[0].name + " and " + (numUsers - 1) + " Others";
 	            } else if (numUsers == 2){
-		        $scope.items[i].currentUsers = $scope.items[i].users[0].name + " and " + $scope.items[i].users[1].name;
+					$scope.items[i].currentUsers = $scope.items[i].users[0].name + " and " + $scope.items[i].users[1].name;
 	            } else if (numUsers == 1) {
-		        $scope.items[i].currentUsers = $scope.items[i].users[0].name;
-                    } else {
-		        $scope.items[i].currentUsers = "";
+					$scope.items[i].currentUsers = $scope.items[i].users[0].name;
+                } else {
+					$scope.items[i].currentUsers = "";
 	            }
-	    });
-            }(i));
+			});
+        }(i));
 	}
     }
 }])
