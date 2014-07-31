ff1ab2cba8201f883709cbb787dbd37504d61f61
Sandy Maguire <sandy@sandymaguire.me>
2014-07-31 10:52:14 -0400

    Prevent play from exploding on bad ssh keys

--- back/app/actors/RepoManagementActor.scala
+++ back/app/actors/RepoManagementActor.scala
@@ -2,6 +2,7 @@ package actors
 
 import play.api._
 import scala.sys.process._
+import scala.io.Source
 import akka.actor.Actor
 import akka.actor.Props
 import akka.event.Logging
@@ -22,12 +23,38 @@ class RepoManagementActor extends Actor {
     }
 
     def initialize = { 
-        Process(Seq(
+        val cmd = Process(Seq(
             "git", 
             "clone", 
+            "-v",
             RepoModel.remote, 
             RepoModel.local
-        )).!!
+        ))
+
+        var lines = Stream[String]();
+        var count = 0;
+        val pio = new ProcessIO(
+            _ => (),
+            stdout => 
+                Source.fromInputStream(stdout).getLines.foreach (
+                    _ => count = count + 1
+                ),
+            stderr => 
+                Source.fromInputStream(stderr).getLines.foreach (
+                    _ => count = count + 1
+                )
+        )
+
+        // okay so this doesn't work great, but you know, whatever
+        val proc = cmd.run(pio)
+        Thread.sleep(5000)
+
+        if (count < 2) {
+            proc.destroy
+            "error: no keys"
+        } else {
+            "success"
+        }
     }
 
     def update = { 
