3f4967fa4e76ea25dd9602fdca655effa8665010
Sandy Maguire <sandy@sandymaguire.me>
2014-08-11 23:47:49 -0400

    Initial steps towards data migration

--- back/app/models/RepoModel.scala
+++ back/app/models/RepoModel.scala
@@ -1,6 +1,13 @@
 package models
 
+import play.api.Play.current
+import play.api.db.slick.DB
+import play.api.db.slick.Config.driver.simple._
 import java.io.File
+import org.joda.time.DateTime
+
+import models._
+import utils._
 
 trait SourceRepositoryModel {
     def initialize: Unit
@@ -8,6 +15,7 @@ trait SourceRepositoryModel {
     def getFilePath(localPath: String) =
         RepoModel.local + File.separator + localPath
     def getFile(localPath: String) = new File(getFilePath(localPath))
+    def getFilePathsInCommit(hash: String): Seq[String]
 }
 
 object RepoModel {
@@ -20,7 +28,47 @@ object RepoModel {
     val defaultBranch = impl.defaultBranch
     def initialize = impl.initialize
     def update = impl.update _
-    def getFilePath = impl.getFilePath _
     def getFile = impl.getFile _
+    def getFilePath = impl.getFilePath _
+    def getFilePathsInCommit(hash: String) = impl.getFilePathsInCommit(hash)
+
+    def fastforward(srcCommit: String, dstCommit: String) = {
+        val Table = TableQuery[SnapshotModel]
+
+        val touchedFiles = getFilePathsInCommit(srcCommit)
+        val linesPerFile = DB.withSession { implicit session =>
+            Table
+            .where(_.file inSet touchedFiles)
+            .where(_.commit === srcCommit)
+            .list
+        }.groupBy(_.file).map { case(file, snaps) =>
+            file -> snaps.flatMap { snap =>
+                snap.lines
+            }.groupBy(line => line).map {
+                x => x._1 -> x._2.length
+            }
+        }
+
+        linesPerFile.map { case(filepath, lines) =>
+            import scala.sys.process._
+            filepath -> Seq(
+                "accio",
+                "--file=" + filepath,
+                "--lines=" + lines.toSeq.sortBy(_._1).mkString
+            ).!!.split("\n").map { x =>
+                // TODO(sandy): make this get the right line numbers
+                x.toInt -> 5
+            }
+        }.map { case(filepath, newlines) =>
+            Snapshot(
+                None,
+                new DateTime(0), // TODO(sandy): get the right time for this
+                filepath,
+                User.getById(1).get, // TODO(sandy): figure out a proper user for this
+                dstCommit,
+                newlines.toSeq.map(_._1) // TODO(sandy): make this use counts also
+            ).insert()
+        }
+    }
 }
 
