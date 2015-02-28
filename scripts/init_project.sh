8e4bb6d82030eeebb5dbf7d2f1d48b5f3c2cdfa2
Sandy Maguire <sandy@sandymaguire.me>
2015-02-28 18:57:37 -0500

    User-friendly install scripts

---
+++ scripts/init_project.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo -n "checking for take2rc... "
+if [ ! -e ./.take2rc ]; then
+    echo "missing"
+    echo -n "enter the port number we gave you: "
+    read PORT
+    echo "http://ec2-52-10-51-63.us-west-2.compute.amazonaws.com:$PORT/" > ./.take2rc
+else
+    echo "found"
+fi
+
+echo -n "checking for accioignore... "
+if [ ! -e ./.accioignore ]; then
+    echo "missing"
+    echo  > ./.accioignore
+else
+    echo "found"
+fi
+
+echo
+echo "project init succeeded!"
---
+++ scripts/install_client.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+echo -n "checking for pathogen... "
+if [ ! -e ~/.vim/bundle ]; then
+    echo "missing"
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+else
+    echo "found"
+fi
+
+get_take2() {
+    svn checkout https://github.com/isovector/take2/trunk/front/vim &> /dev/null
+    mv vim take2
+}
+
+echo -n "checking for vimrc... "
+if [ -e ~/.vimrc ]; then
+    if grep "Plug" ~/.vimrc > /dev/null; then
+        echo "incompatible"
+        echo
+        echo "FATAL ERROR: incompatible package manager"
+        echo "You will need to install the vim plugin manually."
+        echo "Please move ./take2/ into your vim package manager."
+        get_take2
+        echo
+        echo "install client failed"
+        exit 1
+
+    elif grep "pathogen" ~/.vimrc > /dev/null; then
+        echo "pathogen found"
+    else
+        echo "pathogen missing"
+        echo "execute pathogen#infect()" >> ~/.vimrc
+    fi
+
+else
+    echo "missing"
+    cat << EOF > ~/.vimrc
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+EOF
+fi
+
+echo -n "checking for take2... "
+if [ ! -e ~/.vim/bundle/take2 ]; then
+    echo "missing"
+    cd ~/.vim/bundle
+    get_take2
+else
+    echo "found"
+fi
+
+echo
+echo "install client succeeded!"
