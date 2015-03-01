7a019f57f72f496e056ac893b597e747ffaa5bd7
Austin <duricooldude@gmail.com>
2015-02-28 19:52:00 -0500

    Fixed highlighting so it highlights whole symbols

--- back/public/javascripts/file.js
+++ back/public/javascripts/file.js
@@ -56,7 +56,6 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
 
         $("[name='my-checkbox']").bootstrapSwitch();
         $('input[name="my-checkbox"]').bootstrapSwitch('onSwitchChange',(function () {
-            console.log('kkkkk');
             if ($(".file-viewer").css("display") == "none") {
                 $(".file-viewer").css("display", "block");
                 $(".user-viewer").css("display", "none");
@@ -71,7 +70,6 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
         $('input[name="fake-data"]').bootstrapSwitch('onSwitchChange', (function () {
             
             $scope.useFakeData = !$scope.useFakeData;
-            console.log($scope.useFakeData)
             $scope.create_data();
         }));
     }
@@ -79,36 +77,15 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
     $scope.makeUserChart = function(){
         var ctx = $("#userChart").get(0).getContext("2d");
 
-        // TODO - Change this to finding "number" for extensibility
-        //var lineSelected = $("[aria-describedby^='popover']");
-
-        //lineSelected.css("background-color", "#ffffff")
-
-        //var lineNum = parseInt(lineSelected.attr('class').split(/\s+/)[1].substring(6));
-
-        /*var data = _.find($scope.lineItems, function (lineItem) {
-            return lineItem.line == lineNum;
-        }).chartData;
-        console.log(data)*/
-
         var options = {
             legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%> <%=segments[i].value*5%>s<%}%></li><%}%></ul>",
             segmentShowStroke: true,
             animateRotate: true,
             animationEasing: "easeOutQuart"
         }
-        console.log($scope.userChartData.length);
         var myNewChart = new Chart(ctx).Pie($scope.userChartData, options);
         var legend = myNewChart.generateLegend();
         $("#user-legend").html(legend);
-
-        // Popup styling
-        /*console.log($(".popover").css("left"))
-        if ($(".popover").css("left").substring(0, 1) == "-") {
-            console.log("farout"); $(".popover").css("left", "11px");
-        }
-        $(".popover-title").html("Users - Line " + lineNum);
-        $(".arrow").css("display", "none");*/
     }
 
     // Checking for popup on page - modify it with appropriate data when it does
@@ -134,14 +111,11 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
     }
 
     $scope.setFileContents = function (filestuff) {
-        console.log(filestuff);
         //Get our path for breadcrumbs
         $scope.pathArray = filestuff.path.split("/");
         //Insert the code content into the page
         $('#file_brush').html(filestuff.contents);
         $scope.path = filestuff.path;
-        console.log(filestuff.path);
-        console.log($scope.path);
         $scope.create_data();
         $scope.getExpertUsers(filestuff.path);
         $scope.getRelatedFiles(filestuff.path);
@@ -165,7 +139,6 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
         var data = _.find($scope.lineItems, function (lineItem) {
             return lineItem.line == lineNum;
         }).chartData;
-        console.log(data)
         var options = {
             legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%> <%=segments[i].value*5%>s<%}%></li><%}%></ul>",
             segmentShowStroke: true,
@@ -177,9 +150,8 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
         $("#legend").html(legend);
 
         // Popup styling
-        console.log($(".popover").css("left"))
         if ($(".popover").css("left").substring(0, 1) == "-") {
-            console.log("farout"); $(".popover").css("left", "11px");
+            $(".popover").css("left", "11px");
         }
         $(".popover-title").html("Users - Line " + lineNum);
         $(".arrow").css("display", "none");
@@ -212,11 +184,9 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
             
             if (lineItem.line < lines.length / 2 && lineItem.totalCount > 0) {
                
-                console.log(lineItem.line);
-                console.log(lineItem.totalCount)
                 var color = 'background-color:';
 
-                color += colors[Math.floor(lineItem.totalCount/maxLineCount * colors.length)];
+                color += colors[Math.floor(lineItem.totalCount/maxLineCount * colors.length) - 1];
                 color += ' !important';
                
                 lines[lineItem.line - 1].style.cssText = color;
@@ -248,17 +218,6 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
     }
 
     $scope.add_user = function (userData, color) {
-        /*console.log(color)
-        var r = parseInt(color.substring(1, 2), 16);
-        var g = parseInt(color.substring(2, 4), 16);
-        var b = parseInt(color.substring(4, 6), 16);
-       
-        var rt = (r + (0.25 * (255 - r))).toString(16);
-        var gt = (g + (0.25 * (255 - g))).toString(16);
-        var bt = (b + (0.25 * (255 - b))).toString(16);
-        console.log(bt);
-        var highlightColor = '#' + rt.toString(10) + gt.toString(10) + bt.toString(10);*/
-
         var chartItem = {
             label: userData.user.name,
             value: userData.timeSpent,
@@ -271,36 +230,43 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
 
     // Parse data and attach user data to lines of code
     $scope.add_lines = function (userData, color) {
-        console.log("into add lines")
-        console.log(userData);
-
         var userColor = color;
 
         userData.timeSpentByLine.forEach(function (lineItem) {
-            console.log(lineItem);
+            var symbolToFill = _.find($scope.symbolArray, function(symbolObject) {
+                return symbolObject.line == lineItem.line;
+            });
+
             var tempObj;
             var lineIndex;
+            // Make a copy so we can modify the line when we iterate
+            var lineItemForIteration = lineItem;
+            while (lineItemForIteration.line != symbolToFill.endLine + 1) {
+                if ((lineIndex = _.findIndex($scope.lines, { line: lineItemForIteration.line })) != -1) {
+                    // Preparing line to be modified if line already exists
+                    tempObj = $scope.lines[lineIndex];
+                    $scope.lines[lineIndex] = $scope.lines[$scope.lines.length - 1];
+                    $scope.lines[$scope.lines.length - 1] = tempObj;
 
-            if ((lineIndex = _.findIndex($scope.lines, { line: lineItem.line })) != -1) {
-                // Preparing line to be modified if line already exists
-                tempObj = $scope.lines[lineIndex];
-                $scope.lines[lineIndex] = $scope.lines[$scope.lines.length - 1];
-                $scope.lines[$scope.lines.length - 1] = tempObj;
+                } else {
+                    // Creating new line object (only do if line doesn't exist, otherwise get a handle on it...)
+                    var tempObj = new Object();
+                    tempObj.line = lineItemForIteration.line;
+                    tempObj.users = [];
+                    $scope.lines.push(tempObj);
+                }
+                // Creating new user object and pushing on to appropriate line object
+                tempObj = new Object();
+                tempObj.userId = userData.user.id;
+                tempObj.userName = userData.user.name;
+                tempObj.count = lineItemForIteration.count;
+                tempObj.color = userColor;
+                $scope.lines[$scope.lines.length - 1].users.push(tempObj);
 
-            } else {
-                // Creating new line object (only do if line doesn't exist, otherwise get a handle on it...)
-                var tempObj = new Object();
-                tempObj.line = lineItem.line;
-                tempObj.users = [];
-                $scope.lines.push(tempObj);
+                //Increment our line by 1 until we reach the end of the symbol
+                lineItemForIteration.line += 1;
             }
-            // Creating new user object and pushing on to appropriate line object
-            tempObj = new Object();
-            tempObj.userId = userData.user.id;
-            tempObj.userName = userData.user.name;
-            tempObj.count = lineItem.count;
-            tempObj.color = userColor;
-            $scope.lines[$scope.lines.length - 1].users.push(tempObj);
+            
         });
 
         // Add chart data and totalCount properties to the lines
@@ -326,6 +292,24 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
         $scope.add_importance();
         return userColor;
     }
+
+    $scope.add_symbols = function (symbolArray) {
+        $scope.symbolArray = _.sortBy(symbolArray, function(symbol) {
+            return symbol.line;
+        });
+        var lines = $('.line');
+        var lastLine = lines.length / 2
+        // Iterate through the symbols to find the end of each symbol
+        // start at 1 because we will look behind us, and the first symbol has nothing preceding it
+        for (var i = 1; i <= symbolArray.length - 1; i++) {
+            // Set the end to the start of the next one minus 1
+            $scope.symbolArray[i-1].endLine = $scope.symbolArray[i].line - 1;
+            // Last symbol ends at end of file, but we dont know how long so use -1
+            if (i == symbolArray.length -1) {
+                $scope.symbolArray[i].endLine = lastLine;
+            };
+        };
+    }
     
     
     // Get data and send to parser
@@ -333,27 +317,21 @@ frostbite.controller('FileCtrl', ['$scope','$http', '$q', function ($scope, $htt
         $http.get("/api/metrics/all/" + $scope.path).success(function (data) {
             // Fake it til you make it
             if ($scope.useFakeData) {
-                data = { "file": "back/app/controllers/SnapshotController.scala", "commit": "unimplemented", "userData": [{ "user": { "id": 1, "name": "Adam Sils", "email": "silsadam@gmail.com", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 28, "timeSpentByLine": [{ "line": 40, "count": 1 }, { "line": 41, "count": 1 }, { "line": 42, "count": 1 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 2 }, { "line": 46, "count": 1 }, { "line": 47, "count": 1 }, { "line": 48, "count": 2 }, { "line": 49, "count": 2 }, { "line": 50, "count": 2 }, { "line": 51, "count": 2 }, { "line": 52, "count": 2 }, { "line": 53, "count": 1 }, { "line": 54, "count": 1 }, { "line": 55, "count": 1 }, { "line": 56, "count": 1 }, { "line": 57, "count": 1 }, { "line": 58, "count": 1 }, { "line": 59, "count": 1 }, { "line": 60, "count": 1 }] }, { "user": { "id": 3, "name": "Sandy Maguire", "email": "sandy@sandymaguire.me", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 45, "timeSpentByLine": [{ "line": 30, "count": 1 }, { "line": 31, "count": 1 }, { "line": 32, "count": 1 }, { "line": 33, "count": 1 }, { "line": 34, "count": 1 }, { "line": 35, "count": 2 }, { "line": 36, "count": 1 }, { "line": 37, "count": 1 }, { "line": 38, "count": 2 }, { "line": 39, "count": 2 }, { "line": 40, "count": 2 }, { "line": 41, "count": 2 }, { "line": 42, "count": 2 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 1 }, { "line": 6, "count": 1 }, { "line": 7, "count": 1 }, { "line": 8, "count": 1 }, { "line": 9, "count": 1 }, { "line": 10, "count": 1 }, { "line": 11, "count": 1 }, { "line": 12, "count": 1 }, { "line": 13, "count": 1 }, { "line": 14, "count": 1 }, { "line": 15, "count": 1 }, { "line": 16, "count": 1 }, { "line": 17, "count": 1 }, { "line": 18, "count": 1 }] }, { "user": { "id": 2, "name": "Yolo Swagins", "email": "yolo@swag.me", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 8, "timeSpentByLine": [{ "line": 20, "count": 2 }, { "line": 21, "count": 2 }, { "line": 22, "count": 2 }, { "line": 23, "count": 2 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 1 }, { "line": 54, "count": 2 }, { "line": 55, "count": 2 }] }] }
+                data = {"file":"back/app/models/UserModel.scala","commit":"(unimplemented)","userData":[{"user":{"id":1,"name":"Sandy Maguire","email":"sandy@sandymaguire.me","picture":"(unimplemented)","lastActivity":1425163907882},"timeSpent":14,"timeSpentByLine":[{"line":20,"count":16},{"line":78,"count":2},{"line":29,"count":18},{"line":60,"count":18},{"line":77,"count":2},{"line":34,"count":54},{"line":54,"count":18},{"line":66,"count":10},{"line":35,"count":18},{"line":48,"count":18},{"line":16,"count":9},{"line":40,"count":18},{"line":23,"count":17},{"line":36,"count":18}]}],"symbols":[{"id":139,"line":16,"name":"User","kind":"c"},{"id":140,"line":20,"name":"lastActivity","kind":"l"},{"id":141,"line":23,"name":"save","kind":"m"},{"id":142,"line":29,"name":"getExpertise","kind":"m"},{"id":143,"line":34,"name":"User","kind":"c"},{"id":144,"line":35,"name":"T","kind":"T"},{"id":145,"line":36,"name":"Key","kind":"T"},{"id":146,"line":40,"name":"create","kind":"m"},{"id":147,"line":48,"name":"getAll","kind":"m"},{"id":148,"line":54,"name":"rawGet","kind":"m"},{"id":149,"line":60,"name":"getByEmail","kind":"m"},{"id":150,"line":66,"name":"getActiveSince","kind":"m"},{"id":151,"line":77,"name":"implicitUserWrites","kind":"l"},{"id":152,"line":78,"name":"writes","kind":"m"},{"id":153,"line":87,"name":"implicitUserColumnMapper","kind":"m"},{"id":154,"line":92,"name":"UserModel","kind":"c"},{"id":155,"line":93,"name":"id","kind":"m"},{"id":156,"line":94,"name":"name","kind":"m"},{"id":157,"line":95,"name":"email","kind":"m"},{"id":158,"line":96,"name":"lastActivity","kind":"m"},{"id":159,"line":98,"name":"user","kind":"l"},{"id":459,"line":34,"name":"User","kind":"c"},{"id":468,"line":34,"name":"User","kind":"c"}]};
             }
             var colors = Please.make_color({
                 colors_returned: data.userData.length
             });
+            // Add the symbols so we can mark sections as the same
+            $scope.add_symbols(data.symbols);  
+
             for (var i = 0; i < data.userData.length; i++) {
                 $scope.add_lines(data.userData[i], colors[i]);
                 $scope.add_user(data.userData[i], colors[i]);
             }
         }).error(function (data) {
-            if ($scope.useFakeData) {
-                data = { "file": "back/app/controllers/SnapshotController.scala", "commit": "unimplemented", "userData": [{ "user": { "id": 1, "name": "Adam Sils", "email": "silsadam@gmail.com", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 28, "timeSpentByLine": [{ "line": 40, "count": 1 }, { "line": 41, "count": 1 }, { "line": 42, "count": 1 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 2 }, { "line": 46, "count": 1 }, { "line": 47, "count": 1 }, { "line": 48, "count": 2 }, { "line": 49, "count": 2 }, { "line": 50, "count": 2 }, { "line": 51, "count": 2 }, { "line": 52, "count": 2 }, { "line": 53, "count": 1 }, { "line": 54, "count": 1 }, { "line": 55, "count": 1 }, { "line": 56, "count": 1 }, { "line": 57, "count": 1 }, { "line": 58, "count": 1 }, { "line": 59, "count": 1 }, { "line": 60, "count": 1 }] }, { "user": { "id": 3, "name": "Sandy Maguire", "email": "sandy@sandymaguire.me", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 45, "timeSpentByLine": [{ "line": 30, "count": 1 }, { "line": 31, "count": 1 }, { "line": 32, "count": 1 }, { "line": 33, "count": 1 }, { "line": 34, "count": 1 }, { "line": 35, "count": 2 }, { "line": 36, "count": 1 }, { "line": 37, "count": 1 }, { "line": 38, "count": 2 }, { "line": 39, "count": 2 }, { "line": 40, "count": 2 }, { "line": 41, "count": 2 }, { "line": 42, "count": 2 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 1 }, { "line": 6, "count": 1 }, { "line": 7, "count": 1 }, { "line": 8, "count": 1 }, { "line": 9, "count": 1 }, { "line": 10, "count": 1 }, { "line": 11, "count": 1 }, { "line": 12, "count": 1 }, { "line": 13, "count": 1 }, { "line": 14, "count": 1 }, { "line": 15, "count": 1 }, { "line": 16, "count": 1 }, { "line": 17, "count": 1 }, { "line": 18, "count": 1 }] }, { "user": { "id": 2, "name": "Yolo Swagins", "email": "yolo@swag.me", "picture": "unimplemented", "lastActivity": 1406511540074 }, "timeSpent": 8, "timeSpentByLine": [{ "line": 20, "count": 2 }, { "line": 21, "count": 2 }, { "line": 22, "count": 2 }, { "line": 23, "count": 2 }, { "line": 43, "count": 1 }, { "line": 44, "count": 1 }, { "line": 45, "count": 1 }, { "line": 54, "count": 2 }, { "line": 55, "count": 2 }] }] }
-                for (var i = 0; i < data.userData.length; i++) {
-                    $scope.add_lines(data.userData[i], colors[i])
-                    $scope.add_user(data.userData[i], colors[i]);
-                }
-            }
-        });
-
-        
+           
+        });        
     }
-
     $scope.init();
 }]);
