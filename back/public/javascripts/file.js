95eca478e08cd332e82b57a8b1d8c47410764621
ADAM SILS <apsils@uwaterloo.ca>
2015-03-08 19:57:44 -0400

    adding highlighting for multiple languages

--- back/public/javascripts/file.js
+++ back/public/javascripts/file.js
@@ -141,6 +141,46 @@ frostbite.controller('FileCtrl', ['$scope', '$filter', '$http', '$q', '$interval
         }, 500);
     }
 
+    var entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+    };
+
+    function escapeHtml(string) {
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+          return entityMap[s];
+        });
+    };
+
+
+    var brushMap = {
+        "sh" : "bash",
+        "cpp": "cpp",
+        "cc": "cpp",
+        "c": "cpp",
+        "cs": "csharp",
+        "css": "css",
+        "java": "java",
+        "js": "js",
+        "pl": "perl",
+        "txt": "text",
+        "py": "python",
+        "rb": "rails",
+        "scala": "scala",
+        "sql": "sql",
+        "vb": "vb",
+        "xml": "xml"
+    };
+
+    function getBrushType(filename){
+        var brushType = brushMap[filename.split(".").pop()];
+        return typeof(brushType) != "undefined" ? brushType : "plain";
+    }
+
     $scope.setFileContents = function (filestuff) {
         // Get our path for breadcrumbs
         // Splitting path based on deliminating char OS uses
@@ -151,8 +191,12 @@ frostbite.controller('FileCtrl', ['$scope', '$filter', '$http', '$q', '$interval
         } else {
             $scope.pathArray == filestuff.path;
         }
+        
+        // Set highlight type based on filename ending
+        $("#file_brush").attr('class', 'brush: ' + getBrushType(filestuff.name));
+        
         //Insert the code content into the page
-        $('#file_brush').html(filestuff.contents);
+        $('#file_brush').html(escapeHtml(filestuff.contents));
         $scope.path = filestuff.path;
         $scope.create_data();
         $scope.getExpertUsers(filestuff.path);
