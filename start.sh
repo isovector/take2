83c2ae48e62f36764433859dcf2b852935b9a117
Sandy Maguire <sandy@sandymaguire.me>
2015-03-19 09:03:05 -0400

    start.sh is broken so get rid of it

--- start.sh
+++
@@ -1,6 +0,0 @@
- #!/usr/bin/env bash
-
-git pull
-pkill java
-          nohup ./scripts/run_instance.sh sonicle 80 prod > /dev/null
-sleep 10; nohup ./scripts/run_instance.sh accio 9000 prod > /dev/null
